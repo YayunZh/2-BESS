73178a3a78c3b433f43e18a98a3a45c78c5e7e0f
yayunZh <yayun@berkeley.edu>
2024-10-01 01:01:44 -0700

    add Lite-sparse

---
+++ Lite-Sparse/Battery_Xiaofan-main/files/Flatten_battery_stat.m
@@ -0,0 +1,11 @@
+function y = Flatten_battery_stat(CL)
+    mu = CL.Bat{1}.qlim_mu;
+    sigma = CL.Bat{1}.qlim_var;
+    temp = norminv(linspace(1e-4,1-1e-4,CL.Stat.Bat_num+1),CL.Bat{1}.qlim_mu, CL.Bat{1}.qlim_var);   %Normal inverse cumulative distribution function
+    for k = 1:CL.Stat.Bat_num
+       x_int = linspace(temp(k),temp(k+1),100);
+       y_int = normpdf(x_int,CL.Bat{1}.qlim_mu, CL.Bat{1}.qlim_var).*x_int;% p * x
+       CL.Bat{k}.qlim = CL.Stat.Bat_num*trapz(x_int,y_int);% 9 * integral(p(x)x) - expected value
+    end
+    y = CL;
+end
